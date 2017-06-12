52725954f8a4ef1e25e8f707fa067e178b9d5528
Liaizon Wakest <wakest@gmail.com>
2017-06-12 11:31:01 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+function changeToRed() {
+	grabBody.style.background = "";
+	grabBody.style.backgroundColor = "red";
+}
+function changeToBlue() {
+	grabBody.style.background = "";
+	grabBody.style.backgroundColor = "green";
+}
+function changeToGreen() {
+	grabBody.style.background = "";
+	grabBody.style.backgroundColor = "blue";
+}
+
+function changeToRedAndGreen() {
+	grabBody.style.background = "";
+	grabBody.style.background = "linear-gradient(red, green)fixed";
+}
+function changeToGreenAndBlue() {
+	grabBody.style.background = "";
+	grabBody.style.background = "linear-gradient(green, blue)fixed";
+}
+function changeToBlueAndRed() {
+	grabBody.style.background = "";
+	grabBody.style.background = "linear-gradient(blue, red)fixed";
+}
+
+function changeToRedAndGreenAndBlue() {
+	grabBody.style.background = "";
+	grabBody.style.background = "linear-gradient(red, green, blue)fixed";
+}
+function changeToGreenAndBlueAndRed() {
+	grabBody.style.background = "";
+	grabBody.style.background = "linear-gradient(green, blue, red)fixed";
+}
+function changeToBlueAndRedAndGreen() {
+	grabBody.style.background = "";
+	grabBody.style.background = "linear-gradient(blue, red, green)fixed";
+}
+
+
+var grabBody = document.querySelector("body")
